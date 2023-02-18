0410b33da67221e81f5e24c6d134326169502873
ARADHANA02219 <122155639+ARADHANA02219@users.noreply.github.com>
2023-02-18 19:13:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function Insert(){
+  var name=document.getElementById("bname").value;
+  var name=document.getElementById("name").value;
+
+  var table=`<tr>
+       <td>${id}</td>
+       <td>${bname}</td>
+       <td>${name}</td>
+       <td>${issue}</td>
+       <td>${statusbar}</td>
+  
+  `;
+  btn.addEventListener('click',Insert)
+}
